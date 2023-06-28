acc08673b025985106ac1ac4368dcee88261c87e
AGamez1 <adrian_gamez9@hotmail.com>
2023-06-27 22:32:42 -0500

    Built skeleton userRoutes, userController, and auth.ths.

---
+++ src/services/auth.ts
@@ -0,0 +1,44 @@
+import bcrypt from 'bcrypt';
+import { Request } from 'express';
+import jwt from 'jsonwebtoken';
+import { iUser, User } from '../models/User';
+
+const secret = 'May the Force be with you.';
+
+export const hashPassword = async (plainTextPassword: string) => {
+    const saltRound = 12;
+    const hash = await bcrypt.hash(plainTextPassword, saltRound);
+    return hash;
+}
+
+export const comparePasswords = async (plainTextPassword: string, hashPassword: string) => {
+    return await bcrypt.compare(plainTextPassword, hashPassword);
+}
+
+export const signUserToken = async (user: iUser) => {
+    let token = jwt.sign(
+        { userId: user._id },
+        secret,
+        { expiresIn: '1hr' }
+    );
+    return token;
+}
+
+export const verifyUser = async (req: Request) => {
+    const authHeader = req.headers.authorization;
+
+    if (authHeader) {
+        const token = authHeader.split(' ')[1];
+
+        try {
+            let decoded: any = await jwt.verify(token, secret);
+            return await User.findById(decoded.userId);
+        }
+        catch (err) {
+            return null;
+        }
+    }
+    else {
+        return null;
+    }
+}
