8b6fa18b234753e9aaf97a863ed0298bef98b6f6
Devocalpyse <ngl2d9uvg@relay.firefox.com>
2023-06-29 12:21:06 -0700

    getAllEvents functionality

--- src/controllers/eventController.ts
+++ src/controllers/eventController.ts
@@ -1,6 +1,14 @@
-import { RequestHandler } from "express";
+import { RequestHandler } from 'express';
+import { Event } from '../models/event';
 
-export const getAllEvents: RequestHandler = async (req, res, next) => {};
+export const getAllEvents: RequestHandler = async (req, res) => {
+  try {
+    const events = await Event.find().populate('host');
+    res.status(200).json(events);
+  } catch (error) {
+    res.status(404).json(error);
+  }
+};
 
 export const getOneEvent: RequestHandler = async (req, res, next) => {};
 
