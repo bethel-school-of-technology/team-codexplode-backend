50a1e4e80e37ad418a614adbdd8531848a184db0
RobbieMac82 <robvmacdonald@gmail.com>
2023-07-22 19:12:29 -0400

    cors and event route fix

--- src/app.ts
+++ src/app.ts
@@ -18,8 +18,14 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const cors = require('cors');
+const corsOptions = {
+	origin: ['http://localhost:3000', 'http://localhost:8100']
+};
+app.use(cors(corsOptions));
+
 // routes
-app.use('/api/event', eventRoutes);
+app.use('/api/events', eventRoutes);
 app.use('/api/user', userRoutes);
 
 app.use((req: Request, res: Response, next: NextFunction) => {
